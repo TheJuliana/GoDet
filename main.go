0b3dc9346170a3ec0aab1b7024ccfba2e0ea1d2f
TheJuliana <juliana.svetlakova@gmail.com>
2022-06-15 21:12:45 +0200

    testing determinant

--- main.go
+++ main.go
@@ -11,12 +11,13 @@ import (
 var mod = syscall.NewLazyDLL("libmylib.dll")
 var add = mod.NewProc("Addiction")
 var inc = mod.NewProc("AddToMas")
+var det = mod.NewProc("Determinant")
 
 func mainHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("dll: ", mod.Name)
 	ret, _, err := add.Call(3, 4)
 	if err != nil {
-		fmt.Fprintf(w, "<a href='/'>Result %d</a>", ret)
+		fmt.Fprintf(w, "<a href='/'>Sum: %d</a>", ret)
 	}
 }
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
@@ -24,11 +25,21 @@ func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	var numbers [3]int32 = [3]int32{2, 34, 4}
 	ret, _, err := inc.Call(uintptr(unsafe.Pointer(&numbers)), 3)
 	if err != nil {
-		fmt.Fprintf(w, "<a href='/'>Result %d</a>", ret)
+		fmt.Fprintf(w, "<a href='/'>Sums: %d</a>", ret)
 	}
 }
+func infoHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("dll: ", mod.Name)
+	var numbers [4]int32 = [4]int32{2, 2, 4, 5}
+	ret, _, err := det.Call(uintptr(unsafe.Pointer(&numbers)), 4)
+	if err != nil {
+		fmt.Fprintf(w, "<a href='/'>Determinant:  %d</a>", ret)
+	}
+}
+
 func main() {
 	http.HandleFunc("/", mainHandler)
 	http.HandleFunc("/about", aboutHandler)
+	http.HandleFunc("/info", infoHandler)
 	http.ListenAndServe(":8080", nil)
 }
