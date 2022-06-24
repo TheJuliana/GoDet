3a46f8dd3ad357b657e126daf427f51c3375af3e
TheJuliana <juliana.svetlakova@gmail.com>
2022-06-24 16:15:59 +0200

    add main page

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ var add = mod.NewProc("Addiction")
 var inc = mod.NewProc("AddToMas")
 var det = mod.NewProc("Determinant")
 
-func mainHandler(w http.ResponseWriter, r *http.Request) {
+func sumHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("dll: ", mod.Name)
 	ret, _, err := add.Call(3, 4)
 	if err != nil {
@@ -44,20 +44,44 @@ func infoHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-type Matrix struct {
+type Matrix2 struct {
 	a11 int32
 	a12 int32
 	a21 int32
 	a22 int32
 }
 
-type Page struct {
+type Page1 struct {
 	Determinant uintptr
 	Done        bool
 	El11        int32
 	El12        int32
 	El21        int32
 	El22        int32
+}
+type Matrix3 struct {
+	a11 int32
+	a12 int32
+	a13 int32
+	a21 int32
+	a22 int32
+	a23 int32
+	a31 int32
+	a32 int32
+	a33 int32
+}
+type Page2 struct {
+	Determinant uintptr
+	Done        bool
+	El11        int32
+	El12        int32
+	El13        int32
+	El21        int32
+	El22        int32
+	El23        int32
+	El31        int32
+	El32        int32
+	El33        int32
 }
 
 func matrixHandler2(w http.ResponseWriter, r *http.Request) {
@@ -71,7 +95,7 @@ func matrixHandler2(w http.ResponseWriter, r *http.Request) {
 	a12, _ := strconv.ParseInt(r.FormValue("a12"), 10, 32)
 	a21, _ := strconv.ParseInt(r.FormValue("a21"), 10, 32)
 	a22, _ := strconv.ParseInt(r.FormValue("a22"), 10, 32)
-	mat := Matrix{
+	mat := Matrix2{
 		int32(a11),
 		int32(a12),
 		int32(a21),
@@ -84,20 +108,89 @@ func matrixHandler2(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(mat)
 	var numbers = [4]int32{mat.a11, mat.a12, mat.a21, mat.a22}
 	ret, _, err := det.Call(uintptr(unsafe.Pointer(&numbers)), 4)
-	d := Page{ret, true, numbers[0], numbers[1], numbers[2], numbers[3]}
+	d := Page1{ret, true, numbers[0], numbers[1], numbers[2], numbers[3]}
+
 	tmpl.Execute(w, d)
 	if err != nil {
 		fmt.Println("DETERMINANT= ", ret)
 	}
+
 }
 
+func matrixHandler3(w http.ResponseWriter, r *http.Request) {
+	tmpl, _ := template.ParseFiles("matrix3_page.html")
+	if r.Method != http.MethodPost {
+		tmpl.Execute(w, nil)
+		return
+	}
+	r.ParseForm()
+	a11, _ := strconv.ParseInt(r.FormValue("a11"), 10, 32)
+	a12, _ := strconv.ParseInt(r.FormValue("a12"), 10, 32)
+	a13, _ := strconv.ParseInt(r.FormValue("a13"), 10, 32)
+	a21, _ := strconv.ParseInt(r.FormValue("a21"), 10, 32)
+	a22, _ := strconv.ParseInt(r.FormValue("a22"), 10, 32)
+	a23, _ := strconv.ParseInt(r.FormValue("a23"), 10, 32)
+	a31, _ := strconv.ParseInt(r.FormValue("a31"), 10, 32)
+	a32, _ := strconv.ParseInt(r.FormValue("a32"), 10, 32)
+	a33, _ := strconv.ParseInt(r.FormValue("a33"), 10, 32)
+	mat := Matrix3{
+		int32(a11),
+		int32(a12),
+		int32(a13),
+		int32(a21),
+		int32(a22),
+		int32(a23),
+		int32(a31),
+		int32(a32),
+		int32(a33),
+	}
+	fmt.Println("333333")
+	fmt.Println("a11 = ", mat.a11)
+	fmt.Println("a12 = ", mat.a12)
+	fmt.Println("a13 = ", mat.a13)
+	fmt.Println("a21 = ", mat.a21)
+	fmt.Println("a22 = ", mat.a22)
+	fmt.Println("a23 = ", mat.a23)
+	fmt.Println(mat)
+	var numbers = [9]int32{
+		mat.a11, mat.a12, mat.a13,
+		mat.a21, mat.a22, mat.a23,
+		mat.a31, mat.a32, mat.a33,
+	}
+	ret, _, err := det.Call(uintptr(unsafe.Pointer(&numbers)), 9)
+	d := Page2{ret,
+		true,
+		numbers[0],
+		numbers[1],
+		numbers[2],
+		numbers[3],
+		numbers[4],
+		numbers[5],
+		numbers[6],
+		numbers[7],
+		numbers[8],
+	}
+	tmpl.Execute(w, d)
+	if err != nil {
+		fmt.Println("DETERMINANT= ", ret)
+	}
+	http.Redirect(w, r, "/matrix3", http.StatusFound)
+}
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, _ := template.ParseFiles("main_page.html")
+	if r.Method != http.MethodPost {
+		tmpl.Execute(w, nil)
+		return
+	}
+}
 func main() {
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
-	http.HandleFunc("/", matrixHandler2)
-	//http.HandleFunc("/matrix", getDet)
-	http.HandleFunc("/about", aboutHandler)
+	http.HandleFunc("/matrix2", matrixHandler2)
+	http.HandleFunc("/matrix3", matrixHandler3)
+	http.HandleFunc("/", mainHandler)
+	/*http.HandleFunc("/about", aboutHandler)
 	http.HandleFunc("/main", mainHandler)
-	http.HandleFunc("/info", infoHandler)
+	http.HandleFunc("/info", infoHandler)*/
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
